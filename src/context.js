4b163f1510ea8735a5074e643996dbb09e24746e
NikaKhiz <khizambarelinika123@gmail.com>
2022-12-06 06:47:56 +0400

    Provide globalcontext

--- src/context.js
+++ src/context.js
@@ -1,2 +1,43 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [submenuOpen, setSubmenuOpen] = useState(true);
+
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+  const openSubmenu = () => {
+    setSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        submenuOpen,
+        sidebarOpen,
+        openSubmenu,
+        openSidebar,
+        closeSubmenu,
+        closeSidebar,
+        setSidebarOpen,
+        setSubmenuOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
